9941e706c7bba05cde2d0bbc1c40536828465131
Doometnick <40114373+Doometnick@users.noreply.github.com>
2020-07-26 12:34:19 +0200

    Refactored variables

--- gitproject.bat
+++ gitproject.bat
@@ -3,21 +3,25 @@ Call :createproject %~1
 EXIT /B %ERRORLEVEL%
 
 :createproject
-SET foldername="C:\Users\brand\Documents\MyProjects\%~1"
+SET BASE_FOLDER="C:\Users\brand\Documents\MyProjects\"
+SET PROJECT_NAME=%BASE_FOLDER%%~1
+SET GIT_CONNECT_SCRIPT="C:/BAT/github_connect.py"
+SET GITHUB_USER_DOMAIN="https://github.com/Doometnick/"
+SET PYTHON_EXE="C:\Users\brand\Miniconda3\python.exe"
 
-IF EXIST %foldername% (
+IF EXIST %PROJECT_NAME% (
 	echo Folder already exists.
 ) ELSE (
 
-cd /d "C:\Users\brand\Documents\MyProjects"
+cd /d BASE_FOLDER
 md %~1
 cd %~1
 git init .
 git commit -m "initial commit"
-C:\Users\brand\Miniconda3\python.exe "C:/BAT/github_connect.py" %~1
-git remote add origin https://github.com/Doometnick/%~1.git
+%PYTHON_EXE% %GIT_CONNECT_SCRIPT% %~1
+git remote add origin %GITHUB_USER_DOMAIN%%~1.git
 git remote -v
-echo.>"README.md"
+echo.>>"README.md"
 git add "README.md"
 git commit -m "Added readme"
 git push --set-upstream origin master
